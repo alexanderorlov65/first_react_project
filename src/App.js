c6eec177aee3c7d9727ab01d357718328aaf78e1
Alexander <alexorlov.wrk@mail.ru>
2022-05-22 10:50:13 +0300

    Modal window + react-transition-group

--- src/App.js
+++ src/App.js
@@ -7,49 +7,52 @@ import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput"
+import MyModal from "./components/UI/MyModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
 import './styles/App.css'
 
 
 function App() {
 
-    const [posts, setPosts] = useState([
-      {id: 1, title: ' Javascript', body: "Description"},
-      {id: 2, title: ' Javascript12', body: "Description"},
-      {id: 3, title: ' Javascript2', body: "Description"},
-    ])
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: "Description"},
+    {id: 2, title: 'Javascript12', body: "Description"},
+    {id: 3, title: 'Javascript2', body: "Description"},
+  ])
 
-    const [filter, setFilter] = useState({sort: '', query: ''})
-  
-    const sortedPosts = useMemo (() => {
-      if (filter.sort) {
-        return [...posts].sort((a, b)=> a[filter.sort].localeCompare(b[filter.sort]))
-      } 
-      return posts
-    }, [filter.sort, posts])
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false)
 
-    const sortedAndSearchedPosts = useMemo(() => {
-      return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPosts])
+  const sortedPosts = useMemo (() => {
+    if (filter.sort) {
+      return [...posts].sort((a, b)=> a[filter.sort].localeCompare(b[filter.sort]))
+    } 
+    return posts
+  }, [filter.sort, posts])
 
-    const createPost = (newPost) => {
-      setPosts ([...posts, newPost])
-    }
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+  }, [filter.query, sortedPosts])
 
-    const removePost = (post) => {
-      setPosts(posts.filter(p => p.id !== post.id))
-    }
-
-  
-    return (
-      <div className="App">
-        <PostForm create={createPost}/>
-        <hr style={{margin: '15px 0'}}/>
-        <PostFilter filter={filter} setFilter={setFilter}/>
-        {sortedAndSearchedPosts.length>0 
-          ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title="List of posts 1"/> 
-          : <h1 style={{textAlign: 'center'}}>Not Found</h1>}
-      </div>
-    );
+  const createPost = (newPost) => {
+    setPosts ([...posts, newPost])
+    setModal(false)
   }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  return (
+    <div className="App">
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>Create a user</MyButton>
+      <MyModal visible={modal} setVisible={setModal}><PostForm create={createPost}/></MyModal>
+      <hr style={{margin: '15px 0'}}/>
+      <PostFilter filter={filter} setFilter={setFilter}/>
+      {sortedAndSearchedPosts.length>0 
+        ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title="List of posts 1"/> 
+        : <h1 style={{textAlign: 'center'}}>Not Found</h1>}
+    </div>
+  );
+}
 export default App;
