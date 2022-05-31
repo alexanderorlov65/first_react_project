87f4b3635ddd40b1fc8a3cb5d81f0bcdedf1b580
Alexander <alexorlov.wrk@mail.ru>
2022-05-31 21:13:00 +0300

    Custom hook, axios

--- src/App.js
+++ src/App.js
@@ -9,34 +9,27 @@ import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput"
 import MyModal from "./components/UI/MyModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
+import { usePosts } from "./hooks/usePosts";
+import axios from 'axios'
 import './styles/App.css'
 
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript', body: "Description"},
-    {id: 2, title: 'Javascript12', body: "Description"},
-    {id: 3, title: 'Javascript2', body: "Description"},
-  ])
+  const [posts, setPosts] = useState([])
 
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
-
-  const sortedPosts = useMemo (() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b)=> a[filter.sort].localeCompare(b[filter.sort]))
-    } 
-    return posts
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts ([...posts, newPost])
     setModal(false)
+  }
+
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    setPosts(response.data)
   }
 
   const removePost = (post) => {
@@ -45,6 +38,7 @@ function App() {
 
   return (
     <div className="App">
+      <MyButton onClick={fetchPosts}>GET POSTS</MyButton>
       <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>Create a user</MyButton>
       <MyModal visible={modal} setVisible={setModal}><PostForm create={createPost}/></MyModal>
       <hr style={{margin: '15px 0'}}/>
